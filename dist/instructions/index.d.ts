033b4864ed7beaf7cadd5e7745daac5ad5400f1e
RodrigoBurattoRibas <rodrigoburattoribasrbr@gmail.com>
2026-04-05 21:59:17 -0300

    Removendo 2.0

--- dist/instructions/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from "./R_Instruction";
-export * from "./I_Instruction";
-export * from "./S_Instruction";
-export * from "./B_Instruction";
-export * from "./U_Instruction";
-export * from "./J_Instruction";
-export * from "./SYSTEM_Instruction";
-//# sourceMappingURL=index.d.ts.map
